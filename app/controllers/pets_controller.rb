eae401996e0043b247c7cf1ddb93439f5d31bd14
Alyson Pereira <alysonrichard2001@gmail.com>
2023-09-04 17:13:32 -0300

    done new/create pet controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,20 @@
 class PetsController < ApplicationController
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :specie, :breed, :weight)
+  end
 end
