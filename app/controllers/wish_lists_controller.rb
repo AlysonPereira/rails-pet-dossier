22ca34b0fe9c5c888662425baff733cd3238b2ac
Alyson Pereira <alysonrichard2001@gmail.com>
2023-09-05 15:45:24 -0300

    solved wishlist

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,4 +1,6 @@
 class WishListsController < ApplicationController
+  before_action :set_wishlist, only: %i[show destroy edit update]
+
   def index
     @wishlists = WishList.all
   end
@@ -17,22 +19,27 @@ class WishListsController < ApplicationController
     end
   end
 
-  def show
-    @wishlist = WishList.find(params[:id])
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @wishlist.update(wishlist_params)
+    redirect_to wish_lists_path(@wishlists)
   end
 
   def destroy
+    @wishlist.destroy
+    redirect_to wish_lists_path
   end
 
   private
 
+  def set_wishlist
+    @wishlist = WishList.find(params[:id])
+  end
+
   def wishlist_params
-    params.require(:wishlist).permit(:title)
+    params.require(:wish_list).permit(:title)
   end
 end
