9ef426cabfb5eca14a8a9696013c486be922b2ab
Luisa Assis <analuisassis@hotmail.com>
2023-09-05 18:27:31 -0300

    add dossier page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+  end
+
+  def dossier
+    @vaccines = Vaccine.all
+    @medicines = Medicine.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   resources :wish_lists
   resources :pets do
+    
     resources :medicines, only: [:new, :create]
     resources :vaccines, only: [:new, :create]
   end
+  get 'dossier', to: "pages#dossier"
 end
